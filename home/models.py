4e0ae0967aab12bdfb6c3e7e359e0e8de862949a
kalumba Samuel <kalumbasml@gmail.com>
2024-02-27 20:17:12 +0300

    __str__ method in models

--- home/models.py
+++ home/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     name = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.name
 
 class Bible(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     chapter = models.IntegerField()
     verse = models.IntegerField()
 
+    def __str__(self):
+        return self.book+" "+self.chapter+":"+self.verse
