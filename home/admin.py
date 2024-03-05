aa6ae5c28d32f75d53d95e6f79fa0f6b0aa8cb5b
kalumba Samuel <kalumbasml@gmail.com>
2024-03-05 13:34:31 +0300

    nm

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from home.models import Book, Bible
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Bible)
--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     name = models.CharField(max_length=15)
-    
+
     def __str__(self):
         return self.name
 
