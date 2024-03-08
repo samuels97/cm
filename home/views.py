5931465886c4b767a81378abdf76b55f7f6b1071
kalumba Samuel <kalumbasml@gmail.com>
2024-03-08 13:33:20 +0300

    static files

--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello")
+
+
+def Button(text: str, classNames: List[str], onClick=None)->str:
+    css_class_string = " ".join(classNames)
+    return f'<button class="{css_class_string}">{text}</button>'
